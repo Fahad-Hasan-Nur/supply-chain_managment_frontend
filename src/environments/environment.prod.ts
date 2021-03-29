3ab66f6481cc4b2a31b32f8a73ee6b7cd488698e
Fahad Hasan <fahadhasan@Fahads-MacBook-Air.local>
2021-03-29 09:41:35 -0700

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,17 +3,20 @@
 // export const APPLICATION_NAME = '#APPLICATION-NAME#';
 
 /* Live/production deployment configuration variables */
+export const IS_LIVE = true;
+
+/* Copy this file as environment.ts.ts and modify variables as necessary - Imtiaz */
 export const environment = {
   production: true,
-  GATEWAY_URL: '#GATEWAY_URL#',
-  GLOBAL_GATEWAY_URL: '#GLOBAL_GATEWAY_URL#',
-  NOTIFICATION_URL: '#NOTIFICATION_URL#',
-  LOG_IN_API_Endpoint: '#LOG_IN_API_ENDPOINT#',
-  SERVICE_CONTEXT: '#PRJ_SERVICE_CONTEXT#',
-  FILE_SERVICE_CONTEXT: '#FILE_SERVICE_CONTEXT#',
+  //GATEWAY_URL: 'http://localhost:8080/',
+  GATEWAY_URL: 'https://nur-ecommerce-backend.herokuapp.com/',
+  GLOBAL_GATEWAY_URL: 'http://dev-hscm.grp.gov.bd/global/api',
+  NOTIFICATION_URL: 'http://dev-hscm.grp.gov.bd/bcc/notification',
+  LOG_IN_API_Endpoint: 'http://localhost:4200',
   IS_MODAL_OPEN: false
-};
+  //    // "start": "ng serve -c production --host 0.0.0.0 --disableHostCheck",
 
+};
 /**
  * Live and production deployment configuration variables.
  *
