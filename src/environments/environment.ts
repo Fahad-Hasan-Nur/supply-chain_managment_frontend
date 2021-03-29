a4d5de0744f5bc014e939438af3f768a390d935b
Fahad-Hasan-Nur <44541741+Fahad-Hasan-Nur@users.noreply.github.com>
2021-03-29 09:48:40 -0700

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const IS_LIVE = true;
+
+/* Copy this file as environment.ts.ts and modify variables as necessary - Imtiaz */
+export const environment = {
+  production: false,
+  //GATEWAY_URL: 'http://localhost:8080/',
+  GATEWAY_URL: 'https://nur-ecommerce-backend.herokuapp.com/',
+  GLOBAL_GATEWAY_URL: 'http://dev-hscm.grp.gov.bd/global/api',
+  NOTIFICATION_URL: 'http://dev-hscm.grp.gov.bd/bcc/notification',
+  LOG_IN_API_Endpoint: 'http://localhost:4200',
+  SERVICE_CONTEXT: 'grp-prj-service',
+  FILE_SERVICE_CONTEXT:'cmn-service-file-management/cmn-service-file-management',
+  IS_MODAL_OPEN: false
+  //    // "start": "ng serve -c production --host 0.0.0.0 --disableHostCheck",
+
+};
