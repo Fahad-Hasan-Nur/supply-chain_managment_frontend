fff433d75e8e313dd801991a142106820cec16e5
Fahad Hasan <fahadhasan@Fahads-MacBook-Air.local>
2021-04-11 03:21:49 -0700

    code formatting

--- src/app/views/accounts/unverified-transaction/unverified-transaction.component.ts
+++ src/app/views/accounts/unverified-transaction/unverified-transaction.component.ts
@@ -19,24 +19,24 @@ import { DealerViewComponent } from '../component/dealer-view/dealer-view.compon
 export class UnverifiedTransactionComponent implements OnInit {
 
   constructor(
-    private service: DealerService, 
+    private service: DealerService,
     protected dialog: MatDialog,
-    private toastService:ToastService,
-    public adminService:AdminService,
-    public dealerService:DealerService
-    ) { }
+    private toastService: ToastService,
+    public adminService: AdminService,
+    public dealerService: DealerService
+  ) { }
   public transaction: Transaction[] = [];
   public tran: Transaction[] = [];
   public trans: Transaction[] = [];
-  private req:Requisition;
-  private check: number=0;
-  private dealer:User;
-  public loading:boolean=false;
-  public displayedColumns: string[] = ['Transaction Id', 'Status','action'];
+  private req: Requisition;
+  private check: number = 0;
+  private dealer: User;
+  public loading: boolean = false;
+  public displayedColumns: string[] = ['Transaction Id', 'Status', 'action'];
   public dataSource = new MatTableDataSource;
   public showFilters: boolean;
-  @ViewChild(MatPaginator, {static: true}) public paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) public sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) public paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) public sort: MatSort;
 
   public ngOnInit() {
     this.dataSource.sort = this.sort;
@@ -52,147 +52,147 @@ export class UnverifiedTransactionComponent implements OnInit {
   }
 
   public applyFilter(filterValue: any) {
-      this.dataSource.filter = filterValue.value.trim().toLowerCase();
+    this.dataSource.filter = filterValue.value.trim().toLowerCase();
   }
-  public search(data) {  }
+  public search(data) { }
 
   openDialogView(data?) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.data = {
-        transaction: data
+      transaction: data
     };
     this.dialog.open(TransactionViewComponent, dialogConfig);
   }
 
-   public verifyNow(data?) {
-     this.loading=true;
+  public verifyNow(data?) {
+    this.loading = true;
     this.service.completeTransaction(data.id).subscribe
-    (
-      (response) => {
-        this.verifyRequisition(data);        
-      }, (error) => {
-        console.log(error);
-        this.toastService.openSnackBar(success_message.FAILD, this.toastService.ACTION_WRONG, this.toastService.CLASS_NAME_WRONG);
-        this.loading = false;
-      });  
-    }
-    private verifyRequisition(data:Transaction){
-        this.service.getTransactionByRequisitionId(data.requisitionId).subscribe
-        (
-          (res)=>{
-            this.tran=res;
-            this.tran.forEach(element => {
-              if(element.status=="Complete"){
-                this.check=this.check+1;
-              }else{
-                this.check=-100000;
-              }
-            });
-            if(this.check>0){
-              this.changeStatus(data.requisitionId);
-            }
-          },
-          (error)=>{
-            console.log(error);
-          }
-        )
-
-      this.toastService.openSnackBar(success_message.UPDATED_SUCCESSFULLY, this.toastService.ACTION_SUCESS, this.toastService.CLASS_NAME_SUCESS);
-      this.loading = false;
-    }
-    private changeStatus(id:string){
-      this.service.getRequisitionById(id).subscribe
       (
         (response) => {
-          this.req=response;
-          if(this.req.status!="Complete"){
+          this.verifyRequisition(data);
+        }, (error) => {
+          console.log(error);
+          this.toastService.openSnackBar(success_message.FAILD, this.toastService.ACTION_WRONG, this.toastService.CLASS_NAME_WRONG);
+          this.loading = false;
+        });
+  }
+  private verifyRequisition(data: Transaction) {
+    this.service.getTransactionByRequisitionId(data.requisitionId).subscribe
+      (
+        (res) => {
+          this.tran = res;
+          this.tran.forEach(element => {
+            if (element.status == "Complete") {
+              this.check = this.check + 1;
+            } else {
+              this.check = -100000;
+            }
+          });
+          if (this.check > 0) {
+            this.changeStatus(data.requisitionId);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+
+    this.toastService.openSnackBar(success_message.UPDATED_SUCCESSFULLY, this.toastService.ACTION_SUCESS, this.toastService.CLASS_NAME_SUCESS);
+    this.loading = false;
+  }
+  private changeStatus(id: string) {
+    this.service.getRequisitionById(id).subscribe
+      (
+        (response) => {
+          this.req = response;
+          if (this.req.status != "Complete") {
             this.service.verifyRequisition(id).subscribe
-            (
-              (res)=>{
-              },
-              (error)=>{
-                console.log(error);
-              }
-            )
-          } 
+              (
+                (res) => {
+                },
+                (error) => {
+                  console.log(error);
+                }
+              )
+          }
         }, (error) => {
           console.log(error);
 
-        }); 
-        
-    }
-    public deleteTransaction(data:Transaction){
-      this.loading=true;
-      this.dealerService.deleteTransactionById(data.id).subscribe
+        });
+
+  }
+  public deleteTransaction(data: Transaction) {
+    this.loading = true;
+    this.dealerService.deleteTransactionById(data.id).subscribe
       (
         (response) => {
           this.toastService.openSnackBar(success_message.DELETED_SUCCESSFULLY, this.toastService.ACTION_SUCESS, this.toastService.CLASS_NAME_SUCESS);
-          this.loading=false
-          
+          this.loading = false
+
         }, (error) => {
           console.log(error);
           this.updateRequisition(data.requisitionId);
-        }); 
+        });
 
-    }
-    public contactDealer(data:Transaction){
-      this.adminService.getAdminById(data.userId).subscribe
-        (
-          (res)=>{
-            this.dealer=res; 
-            const dialogConfig = new MatDialogConfig();
-            dialogConfig.disableClose = true;
-            dialogConfig.autoFocus = true;
-            dialogConfig.data = {
-              dealer: this.dealer
-            };  
-            this.dialog.open(DealerViewComponent, dialogConfig);
-          },
-          (error)=>{
-            console.log(error);
-          }
-        )
-    }
-    private updateRequisition(id:string){
+  }
+  public contactDealer(data: Transaction) {
+    this.adminService.getAdminById(data.userId).subscribe
+      (
+        (res) => {
+          this.dealer = res;
+          const dialogConfig = new MatDialogConfig();
+          dialogConfig.disableClose = true;
+          dialogConfig.autoFocus = true;
+          dialogConfig.data = {
+            dealer: this.dealer
+          };
+          this.dialog.open(DealerViewComponent, dialogConfig);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+  private updateRequisition(id: string) {
 
-      this.dealerService.getTransactionByRequisitionId(id).subscribe
+    this.dealerService.getTransactionByRequisitionId(id).subscribe
       (
         (response) => {
-          this.trans=response;
-          if(this.trans.length<1){
+          this.trans = response;
+          if (this.trans.length < 1) {
             this.updateOnCartRequisition(id);
           }
-          this.loading=false    
+          this.loading = false
         }, (error) => {
           console.log(error);
-        }); 
-    }
-    private updateOnCartRequisition(id:string){
-      this.service.getRequisitionById(id).subscribe
+        });
+  }
+  private updateOnCartRequisition(id: string) {
+    this.service.getRequisitionById(id).subscribe
       (
         (response) => {
-          this.req=response;
+          this.req = response;
           this.updateOnCart(this.req);
         }, (error) => {
           console.log(error);
-          this.loading=false;
+          this.loading = false;
 
-        }); 
-    }
-    private updateOnCart(data:Requisition){
-      this.req.status="cart";
-        this.service.updateRequisition(data).subscribe
-          (
-            (response) => {
-              this.loading=false;
-            }, (error) => {
-              this.loading=false;
+        });
+  }
+  private updateOnCart(data: Requisition) {
+    this.req.status = "cart";
+    this.service.updateRequisition(data).subscribe
+      (
+        (response) => {
+          this.loading = false;
+        }, (error) => {
+          this.loading = false;
 
-              console.log(error);
-            });
-      }
-    
+          console.log(error);
+        });
+  }
+
 }
 
